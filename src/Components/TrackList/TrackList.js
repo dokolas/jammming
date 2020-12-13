8f8ad383eb99b89a1c2f74ab5cdcf1ddbe0eacf3
Spiro Dokolas <dokolas@spiros-mbp.lan>
2020-12-12 21:52:48 -0600

    Got Components Passing Correct Array

--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import {Track} from '../Track/Track';
+import './TrackList.css';
 
 export class TrackList extends React.Component{
     render(){
         return (
-            <div className="TrackList">
-                <Track />
+            <div class="TrackList">
+                {
+                    this.props.tracks.map(track => {
+                        return <Track track={track} key={track.id} />
+                    })
+                }
             </div>
         )
     }

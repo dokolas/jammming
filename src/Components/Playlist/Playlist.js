5475bd2c0826539914f4afa5d086f5b4276c1834
Spiro Dokolas <dokolas@gmail.com>
2020-12-12 22:22:05 -0600

    Playlist Passed

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './Playlist.css';
 import {TrackList} from '../TrackList/TrackList';
 
 export class Playlist extends React.Component {
@@ -6,7 +7,7 @@ export class Playlist extends React.Component {
         return (
             <div className="Playlist">
                 <input defaultValue={"New Playlist"}/>
-                {/* <TrackList /> */}
+                <TrackList tracks={this.props.playlistTracks} />
                 <button className="Playlist-save">SAVE TO SPOTIFY</button>
             </div>
         )
